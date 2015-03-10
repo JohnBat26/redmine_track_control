7a461856038ae4145fb69023b3ad669df7662520
Djiby Thiaw <djibythiaw@gmail.com>
2015-03-10 01:20:50 +0000

    Modification style + correction bug sur plugin redmine_track

--- lib/redmine_track_control/issue_query_patch.rb
+++ lib/redmine_track_control/issue_query_patch.rb
@@ -9,7 +9,7 @@ module RedmineTrackControl
       base.class_eval do
       unloadable # Send unloadable so it will not be unloaded in development
 
-      alias_method_chain :available_filters, :trackcontrol
+      #alias_method_chain :available_filters, :trackcontrol
       end
     end
 
